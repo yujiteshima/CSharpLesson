30df511ce36d9d01ea04ec9f709caac0c0252ddf
Yuji Teshima <yuji.teshima@gmail.com>
2020-09-11 23:49:35 +0900

    firstcommi

---
+++ SelfCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace SelfCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World! {0} さん！", name);
+        }
+    }
+}
